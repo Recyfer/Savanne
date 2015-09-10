769e0a3baf29860af4790640ec4dc8ba4a97e293
Recyfer <emilmylius@gmail.com>
2015-09-10 15:09:35 +0200

    First commit. Added some classes/variables.

---
+++ SavanneProjekt2/Savanne/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavanneProjekt2
+{
+    class Savanne
+    {
+
+    }
+
+    abstract class Animal
+    {
+        private double weight;
+        private bool isAlive;
+        private char gender;
+
+        public void Move()
+        {
+            
+        }
+
+        public void Eat(double weight)
+        {
+            
+        }
+
+    }
+
+    class Lion : Animal
+    {
+        
+    }
+
+    class Rabbit : Animal
+    {
+        
+    }
+}
