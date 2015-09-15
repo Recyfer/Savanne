98792a1fd6cf1f89b26fdf45626e0b4686ba8bf3
Recyfer <emilmylius@gmail.com>
2015-09-15 15:05:41 +0200

    Added more methods.

--- SavanneProjekt2/Savanne/Class1.cs
+++ SavanneProjekt2/Savanne/Class1.cs
@@ -11,22 +11,42 @@ namespace SavanneProjekt2
     {
         public Field[,] field = new Field[20, 20];
 
+        public Savannah()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    field[i,j] = new Field();
+                }
+            }
+        }
+
         public void removeAnimal(int x, int y)
         {
             this.field[x, y].animal = null;
+        }
+
+        public void removeGrass(int x, int y)
+        {
+            this.field[x, y].grass = null;
         }
 
         public void addAnimal(int x, int y, Animal animal)
         {
             this.field[x, y].animal = animal;
         }
+
+        public void addGrass(int x, int y, Grass grass)
+        {
+            this.field[x, y].grass = grass;
+        }
+
     }
     class Field
     {
         public Animal animal;
         public Grass grass;
-
-        
     }
 
     abstract class Animal
@@ -36,13 +56,16 @@ namespace SavanneProjekt2
         private bool gender;
         private int posX;
         private int posY;
-        private Random newPosX = new Random();
-        private Random newPosY = new Random();
+        private Random newPosX;
+        private Random newPosY;
+
         private Savannah savannah;
 
         protected Animal(Savannah s)
         {
             savannah = s;
+            newPosX = new Random();
+            newPosY = new Random();
         }
 
         public void Move(Animal animal)
@@ -52,26 +75,78 @@ namespace SavanneProjekt2
                 savannah.removeAnimal(posX, posY);
                 posX += newPosX.Next(-1, 2);
                 posY += newPosY.Next(-1, 2);
-                savannah.addAnimal(posX,posY,this);
+                savannah.addAnimal(posX, posY, this);
             }
         }
 
-        public void Eat(double unitWeight)
+        public void Eat()
         {
-            weight += unitWeight * 200;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = 1; j < 2; j++)
+                {
+                    if (savannah.field[posX + i, posY + j].animal != null)
+                    {
+                        if (this is Lion && savannah.field[posX + i, posY + j].animal is Rabbit)
+                        {
+                            this.weight += savannah.field[posX + i, posY + j].animal.weight;
+                            savannah.removeAnimal(posX + i, posY + j);
+                        }
+
+                    }
+                    else if (this is Rabbit && savannah.field[posX + i, posY + j].grass != null)
+                    {
+                        this.weight += savannah.field[posX + i, posY + j].animal.weight;
+                        savannah.removeGrass(posX + i, posY + j);
+                    }
+                }
+            }
         }
+
+        public void Mate()
+        {
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = 1; j < 2; j++)
+                {
+                    if (savannah.field[posX + i, posY + j].animal != null)
+                    {
+                        if (this is Lion && savannah.field[posX + i, posY + j].animal is Lion)
+                        {
+                            savannah.removeAnimal(posX + i, posY + j);
+                            // finish mating method
+                        }
+                        if (this is Rabbit && savannah.field[posX + i, posY + j].animal is Rabbit)
+                        {
+                            savannah.removeAnimal(posX + i, posY + j);
+                            // finish mating method
+                        }
+                    }
+                }
+            }
+        }
+
+
 
 
     }
 
     class Lion : Animal
     {
+        public Lion(Savannah s)
+            : base(s)
+        {
 
+        }
     }
 
     class Rabbit : Animal
     {
+        public Rabbit(Savannah s)
+            : base(s)
+        {
 
+        }
     }
 
     class Grass
@@ -96,7 +171,7 @@ namespace SavanneProjekt2
                 weight *= 1.1;
             }
 
-            
+
         }
     }
 }
